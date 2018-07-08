32ebc057a9b86234fc651f6c186bc99730fc3c6b
Anish110910 <anish.mendiratta@gmail.com>
2018-07-09 04:04:54 +0530

    basic layout

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Grid, Row, Col ,Button} from 'react-bootstrap';
+import './App.css';
+import {Header} from './components';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+         <Header />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
